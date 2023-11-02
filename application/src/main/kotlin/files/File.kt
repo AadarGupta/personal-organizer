b8df39f07cab20e3424fb8afdb39349fb4696e31
Aadar Gupta <aadar100@Aadars-Macbook-Pro.local>
2023-11-02 15:52:50 -0400

    Buttons for create and delete files + dialog for creating a new file

--- application/src/main/kotlin/files/File.kt
+++ application/src/main/kotlin/files/File.kt
@@ -5,17 +5,19 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
-
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 
 class File(filename: String) {
     var isFolder = false
@@ -42,8 +44,12 @@ fun FileListItem(file: File, fileLevel: MutableState<String>) {
 
     TextButton(
         onClick = { fileClicked.value = !fileClicked.value },
-        modifier = Modifier.height(50.dp)
-    ) {
+        modifier = Modifier.height(50.dp)) {
+    val showFileEdit = remember { mutableStateOf(false) }
+    if (showFileEdit.value) {
+        FileEdit(file, showFileEdit)
+    }
+
         if (file.isFolder) {
             Image(
                 painter = painterResource("folderIcon.png"),
@@ -58,8 +64,7 @@ fun FileListItem(file: File, fileLevel: MutableState<String>) {
         Text(
             text = file.filename,
             fontSize = 20.sp,
-            modifier = Modifier.padding(horizontal = 50.dp, vertical = 0.dp),
-            color = Color.Black,
+            color = Color.Black
         )
     }
 }
@@ -74,7 +79,7 @@ fun FileEdit(file: File, showEditView: MutableState<Boolean>) {
             onCloseRequest = {
                 isAskingToClose.value = !isAskingToClose.value
                 showEditView.value = !showEditView.value
-            }
+            }, title="Editing: ${file.filename}"
         ) {
             var query = remember { mutableStateOf(file.content) }
             Column(
@@ -108,6 +113,69 @@ fun FileEdit(file: File, showEditView: MutableState<Boolean>) {
             }
         }
     }
+}
+
+// Function should create a new file
+fun createNewFile(filename: String) {
+    println("Creating a new file called '${filename}'...")
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FileCreate(
+    active: MutableState<Boolean>
+) {
+    var item by remember { mutableStateOf(TextFieldValue("")) }
+
+    AlertDialog(
+        title = {
+            Text(
+                text = "Create New File", modifier = Modifier.padding(20.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        },
+        onDismissRequest = { active.value = false },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    //toDoVM.editToDoList(toDoItem, item.text)
+                    active.value = false
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { active.value = false }
+            ) {
+                Text("Dismiss")
+            }
+        },
+        text = {
+            Column(
+                modifier = Modifier.padding(10.dp),
+            ) {
+
+                TextField(
+                    value = item, onValueChange = { newText ->
+                        item = newText
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+
+        },
+    )
 }
 
 
--- application/src/main/kotlin/files/FileStructure.kt
+++ application/src/main/kotlin/files/FileStructure.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -20,6 +21,9 @@ import org.jetbrains.exposed.sql.transactions.transaction
 fun FileItemList(fileLevel: MutableState<String>) {
 
     var fileList = mutableListOf<File>();
+
+    var toCreate = remember { mutableStateOf(false) }
+    var toDelete = remember{ mutableStateOf(false) }
 
     transaction {
         for (file in FileDataObject.selectAll()) {
@@ -38,6 +42,41 @@ fun FileItemList(fileLevel: MutableState<String>) {
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(5.dp)
     ) {
+
+        Row( modifier = Modifier.padding(horizontal = 0.dp, vertical = 0.dp),) {
+            TextButton(onClick = { toCreate.value = !toCreate.value }, modifier = Modifier.height(50.dp)) {
+                if(toCreate.value) {
+                    FileCreate(toCreate)
+                }
+                Image(
+                    painter = painterResource("createFileIcon.png"),
+                    contentDescription = "Create New File Icon",
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Create New File",
+                    fontSize = 15.sp,
+                    color = Color.Blue,
+                )
+            }
+
+            TextButton(onClick = { toDelete.value = !toDelete.value }, modifier = Modifier.height(50.dp)) {
+                /*if(toCreate.value) {
+                    // Needs to be FileDelete which pulls a list of files and deletes the clicked ones
+                    FileCreate(toCreate)
+                }*/
+                Image(
+                    painter = painterResource("trashIcon.png"),
+                    contentDescription = "Delete File Icon",
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Delete File",
+                    fontSize = 15.sp,
+                    color = Color.Red,
+                )
+            }
+        }
 
         if (fileLevel.value != "root") {
             Box(
