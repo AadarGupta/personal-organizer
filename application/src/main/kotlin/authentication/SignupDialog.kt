0450a6bc7265922a911eb20756f3bfe367d7a9e5
pvico12 <petarvico3@outlook.com>
2023-11-28 19:42:33 -0500

    add show/hide functionality for password

--- application/src/main/kotlin/authentication/SignupDialog.kt
+++ application/src/main/kotlin/authentication/SignupDialog.kt
@@ -1,15 +1,20 @@
 package authentication
 
 import MyHttp
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,6 +30,8 @@ fun SignupDialog(
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var usernameInUse by remember { mutableStateOf(false) }
+
+    var passwordVisibility: Boolean by remember { mutableStateOf(false) }
 
     AlertDialog(
         title = {
@@ -78,6 +85,7 @@ fun SignupDialog(
                             username = newText
                         },
                         maxLines = 1,
+                        placeholder = { Text("Enter username") },
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 20.dp),
@@ -97,6 +105,7 @@ fun SignupDialog(
                             password = newText
                         },
                         maxLines = 1,
+                        placeholder = { Text("Enter password") },
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 20.dp),
@@ -105,7 +114,28 @@ fun SignupDialog(
                             focusedIndicatorColor = Color.Transparent,
                             unfocusedIndicatorColor = Color.Transparent,
                             disabledIndicatorColor = Color.Transparent
-                        )
+                        ),
+                        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                        trailingIcon = {
+                            val passwordVisibilityImage = if (passwordVisibility)
+                                "hidePassword.png"
+                            else "showPassword.png"
+
+                            // Please provide localized description for accessibility services
+                            val description = if (passwordVisibility) "Hide password" else "Show password"
+
+                            IconButton(
+                                onClick = {passwordVisibility = !passwordVisibility},
+                                modifier = Modifier
+                                    .padding(end = 12.dp)
+                                    .width(20.dp)
+                            ){
+                                Image(
+                                    painter = painterResource(passwordVisibilityImage),
+                                    contentDescription = description
+                                )
+                            }
+                        }
                     )
                     if (usernameInUse) {
                         Text(
