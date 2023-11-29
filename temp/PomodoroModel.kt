53992c347f5596546ffccd1fffb68a796e75607b
cajpark <jp@intravision.ca>
2023-11-28 22:24:28 -0500

    added editable time

--- temp/PomodoroModel.kt
+++
@@ -1,6 +0,0 @@
-package sidebar.pomodoro
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PomodoroModel(val id: Int, val breaktime: Int, val worktime: Int, val isChecked: Boolean) {}
